e5d6367438f68cceb198e1fb50e55dc91e211199
HP <cherrat.nour@gmail.com>
2020-06-14 03:07:36 +0100

    about : admin

---
+++ src/Entity/Presentation.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PresentationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PresentationRepository::class)
+ */
+class Presentation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $about;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $mission;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $vision;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $histoire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAbout(): ?string
+    {
+        return $this->about;
+    }
+
+    public function setAbout(string $about): self
+    {
+        $this->about = $about;
+
+        return $this;
+    }
+
+    public function getMission(): ?string
+    {
+        return $this->mission;
+    }
+
+    public function setMission(string $mission): self
+    {
+        $this->mission = $mission;
+
+        return $this;
+    }
+
+    public function getVision(): ?string
+    {
+        return $this->vision;
+    }
+
+    public function setVision(string $vision): self
+    {
+        $this->vision = $vision;
+
+        return $this;
+    }
+
+    public function getHistoire(): ?string
+    {
+        return $this->histoire;
+    }
+
+    public function setHistoire(string $histoire): self
+    {
+        $this->histoire = $histoire;
+
+        return $this;
+    }
+}
